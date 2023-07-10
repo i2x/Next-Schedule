bc17f1d9412495be4ea3d1d4bb742ab4268af087
Rx <0x8084@gmail.com>
2023-07-10 03:30:26 +0000

    fix wrong date and format

--- background.js
+++ background.js
@@ -1,40 +1,40 @@
 async function updateBadge() {
-  let response = await fetch("schedule.json");
-  let schedule = await response.json();
+    let response = await fetch("schedule.json");
+    let schedule = await response.json();
 
-  let now = new Date();
-  let current_day_of_week = now.getUTCDay();
-  let current_hour = now.getUTCHours();
-  let current_minutes = now.getUTCMinutes();
+    let now = new Date();
+    let current_day_of_week = now.getUTCDay();
+    let current_hour = now.getUTCHours();
+    let current_minutes = now.getUTCMinutes();
 
-  let current_total_minutes =
-    current_day_of_week * 1440 + current_hour * 60 + current_minutes;
-  let schedule_in_minutes = schedule.map(
-    ({ day, start }) => day * 1440 + start * 60
-  );
-  let diffs = schedule_in_minutes.map(
-    (item) =>
-      (item < current_total_minutes ? item + 7 * 1440 : item) -
-      current_total_minutes
-  );
-  let min_diff = Math.min(...diffs);
-  let current_index = diffs.indexOf(min_diff);
+    let current_total_minutes =
+        current_day_of_week * 1440 + current_hour * 60 + current_minutes;
+    let schedule_in_minutes = schedule.map(
+        ({ day, start }) => day * 1440 + start * 60
+    );
+    let diffs = schedule_in_minutes.map(
+        (item) =>
+            (item < current_total_minutes ? item + 7 * 1440 : item) -
+            current_total_minutes
+    );
+    let min_diff = Math.min(...diffs);
+    let current_index = diffs.indexOf(min_diff);
 
-  let min_diff_hours = Math.floor(min_diff / 60);
-  let min_diff_minutes = min_diff % 60;
-  let time_to_next_schedule = `${min_diff_hours
-    .toString()
-    .padStart(2, "0")}:${min_diff_minutes.toString().padStart(2, "0")}`;
+    let min_diff_hours = Math.floor(min_diff / 60);
+    let min_diff_minutes = min_diff % 60;
+    let time_to_next_schedule = `${min_diff_hours
+        .toString()
+        .padStart(2, "0")}:${min_diff_minutes.toString().padStart(2, "0")}`;
 
-  chrome.action.setBadgeText({ text: time_to_next_schedule });
-  // chrome.storage.local.set({subject_info: schedule[current_index]});
+    chrome.action.setBadgeText({ text: time_to_next_schedule });
+    // chrome.storage.local.set({subject_info: schedule[current_index]});
 
-  chrome.storage.local.set({
-    data: {
-      schedule: schedule,
-      current_index: current_index,
-    },
-  });
+    chrome.storage.local.set({
+        data: {
+            schedule: schedule,
+            current_index: current_index,
+        },
+    });
 }
 
 chrome.action.setBadgeBackgroundColor({ color: "#282C34" }); // set background color
@@ -44,12 +44,12 @@ chrome.runtime.onStartup.addListener(updateBadge);
 
 // Update the badge when the extension is installed or updated
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.alarms.create("updateBadge", { periodInMinutes: 1 });
+    chrome.alarms.create("updateBadge", { periodInMinutes: 1 });
 });
 
 // Update the badge periodically
 chrome.alarms.onAlarm.addListener((alarm) => {
-  if (alarm.name === "updateBadge") {
-    updateBadge();
-  }
+    if (alarm.name === "updateBadge") {
+        updateBadge();
+    }
 });
--- popup.js
+++ popup.js
@@ -1,36 +1,34 @@
 let current_index = 0;
 let schedule = [];
-
 async function getStoredData() {
-  let store = await chrome.storage.local.get(["data"]);
-  schedule = store.data.schedule;
-  current_index = store.data.current_index;
+    let store = await chrome.storage.local.get(["data"]);
+    schedule = store.data.schedule;
+    current_index = store.data.current_index;
 }
-
 async function render() {
-  let result = schedule[current_index];
-  document.getElementById(
-    "subject-name"
-  ).innerText = `${result.name}\n${result.id} S.${result.sec}\n${result.room} \n${result.date}`;
+    let result = schedule[current_index];
+    document.getElementById(
+        "subject-name"
+    ).innerText = `${result.name}\n${result.id} S.${result.sec}\n${result.room} \n${result.date}`;
 }
 
 document.getElementById("btnBack").addEventListener("click", async function () {
-  current_index = Math.max(current_index - 1, 0);
-  await render();
+    current_index = Math.max(current_index - 1, 0);
+    await render();
 });
 
 document.getElementById("btnNow").addEventListener("click", async function () {
-  let store = await chrome.storage.local.get(["data"]);
-  current_index = store.data.current_index;
-  await render();
+    let store = await chrome.storage.local.get(["data"]);
+    current_index = store.data.current_index;
+    await render();
 });
 
 document.getElementById("btnNext").addEventListener("click", async function () {
-  current_index = Math.min(current_index + 1, schedule.length - 1);
-  await render();
+    current_index = Math.min(current_index + 1, schedule.length - 1);
+    await render();
 });
 
 (async function init() {
-  await getStoredData();
-  await render();
+    await getStoredData();
+    await render();
 })();
